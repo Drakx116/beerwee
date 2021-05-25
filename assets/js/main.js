27a3227e5d034b88063b89fdce70383b8cdf83c2
Drakx116 <alexandre2.guyot@gmail.com>
2021-05-25 11:17:14 +0200

    Entrance - Improve slide animation with async call

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,24 +3,21 @@ const main = document.getElementById('main');
 const validate = document.getElementById('validate-entrance');
 const illus = document.getElementById('entrance-illus');
 
-validate.onclick = e => {
+validate.onclick = async e => {
     document.body.style.overflow = 'hidden';
     illus.style.display = 'none';
 
-    slideUp(entrance, 750).then(() => {
-        slideDown(main, 500).then(() => {
-            document.body.style.overflow = 'auto';
-        });
-    });
+    await slideUp(entrance, 1200);
+    document.body.style.overflow = 'auto';
 }
+
+
 
 /////////////////
 // ANIMATIONS //
 ///////////////
 
-
-// HIDE
-let slideUp = async (target, duration = 500) => {
+async function slideUp(target, duration = 500) {
     target.style.transitionProperty = 'height, margin, padding';
     target.style.transitionDuration = duration + 'ms';
     target.style.boxSizing = 'border-box';
@@ -31,6 +28,9 @@ let slideUp = async (target, duration = 500) => {
     target.style.paddingBottom = 0 + 'px';
     target.style.marginTop = 0 + 'px';
     target.style.marginBottom = 0 + 'px';
+
+    // Immediately displays the main section
+    main.style.display = 'flex';
 
     window.setTimeout( () => {
         target.style.display = 'none';
@@ -44,34 +44,5 @@ let slideUp = async (target, duration = 500) => {
         target.style.removeProperty('transition-property');
     }, duration);
 
-    return true;
-}
-
-// SHOW
-const slideDown = async (target, duration = 500) => {
-    target.style.display = 'flex';
-    target.style.overflow = 'hidden';
-    target.style.height = '0px';
-    target.style.paddingTop = '0px';
-    target.style.paddingBottom = '0px';
-    target.style.marginTop = '0px';
-    target.style.marginBottom = '0px';
-    target.offsetHeight;
-    target.style.boxSizing = 'border-box';
-    target.style.transitionProperty = "height, margin, padding";
-    target.style.transitionDuration = duration + 'ms';
-    target.style.height = target.offsetHeight + 'px';
-    target.style.removeProperty('padding-top');
-    target.style.removeProperty('padding-bottom');
-    target.style.removeProperty('margin-top');
-    target.style.removeProperty('margin-bottom');
-
-    window.setTimeout( () => {
-        target.style.removeProperty('height');
-        target.style.removeProperty('overflow');
-        target.style.removeProperty('transition-duration');
-        target.style.removeProperty('transition-property');
-    }, 250);
-
-    return true;
+    return new Promise(resolve => { setTimeout(resolve, duration) });
 }
